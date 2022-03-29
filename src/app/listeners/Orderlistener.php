c5573c3cfbf8062a8d0c1498e448d24a747f2f85
= <=>
2022-03-29 16:31:07 +0530

    onCreated added

--- src/app/listeners/Orderlistener.php
+++ src/app/listeners/Orderlistener.php
@@ -24,6 +24,21 @@ class Orderlistener extends Injectable
         die();
         $this->logger->info("from order After notifications");
     }
+    public function onCreated(Event $event, \App\Components\Orderscomponent $component, $postData)
+    {
+        // print_r($postData);
+        $setting = new Settings();
+        $settingData = $setting::findFirst('1');
+        // if zipcode is empty
+        if (!strlen($postData->zipcode)) {
+            // echo "zip is not set look for the default value";
+            $postData->zipcode = $settingData->default_zipcode;
+        }
+        // echo "🔫 died in order listener";
+        $postData->save();
+        die();
+        $this->logger->info("from order After notifications");
+    }
     // public function beforeCreate(Event $event, \App\Components\Orderscomponent $component)
     // {
     //     $this->logger->info("from order Before notifications");
--- src/app/listeners/Productlistener.php
+++ src/app/listeners/Productlistener.php
@@ -31,6 +31,28 @@ class Productlistener extends Injectable
         die();
         $this->logger->info("from Product After notifications");
     }
+    public function onCreated(Event $event, \App\Components\Producstcomponent $component, $postData)
+    {
+        $setting = new Settings();
+        $settingData = $setting::findFirst('1');
+        // if with tag is set
+        if ($settingData->title_optimization == "with_tag") {
+            $postData->name = implode(explode(" ", $postData->name)) . implode(explode(",", $postData->tags));
+        }
+
+        // if product price is empty or zero then set default
+        if ($postData->price == 0 || !strlen($postData->price)) {
+            $postData->price = $settingData->default_price;
+        }
+        // if product quantity is empty or zero then set default
+        if ($postData->stock == 0 || !strlen($postData->stock)) {
+            $postData->stock = $settingData->default_stock;
+        }
+        // echo "🔫 died in order listener";
+        $postData->save();
+        die();
+        $this->logger->info("from Product After notifications");
+    }
     // public function beforeCreate(Event $event, \App\Components\Orderscomponent $component)
     // {
     //     $this->logger->info("from order Before notifications");
